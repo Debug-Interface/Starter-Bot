3cd35513208fc7cb99bef9d731c8806c67fb8ab2
Brian Leyva <55462869+xxSShadow@users.noreply.github.com>
2022-01-19 14:57:45 -0800

    Delete Index.js

--- Index.js
+++
@@ -1,5 +0,0 @@
-const { Client, Intents } = require('discord.js);
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-client.once('ready', () => {});
-client.on('messageCreate', (message) => {});
-client.login('TOKEN');
