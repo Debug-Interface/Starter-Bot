5273b4b19226f08a90b562bf872846a21d2a2f01
Brian Leyva <55462869+xxSShadow@users.noreply.github.com>
2022-02-04 13:00:17 -0800

    Update Help.js

--- Commands/Help.js
+++ Commands/Help.js
@@ -1,5 +1,19 @@
+const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
+const button1 = new MessageButton().setCustomId('primary').setLabel('Primary').setStyle('PRIMARY')
+
 module.exports = ({
   name: 'Help',
   description: 'This Is A Help Command!',
-  execute (message) {}
+  execute (message) {
+
+    Embed1 = new MessageEmbed()
+    .setColor('#0099ff')
+    .setTitle('Title')
+    .setDescription('Description')
+    .setTimestamp()
+    
+    Row = new MessageActionRow().addComponents( button1 );
+    
+    message.channel.send({ embeds: [ Embed1 ], components: [ Row ] });
+  }
 });
